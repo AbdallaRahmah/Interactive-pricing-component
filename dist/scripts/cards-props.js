7c250265e297d91b97ac603c2603ca67118b198c
Abdalla <abdallamustafa57@gmail.com>
2022-04-28 02:06:55 +0200

    create and modify the header section and card props section and style the fotter section

---
+++ dist/scripts/cards-props.js
@@ -0,0 +1,19 @@
+let items = [`Unlimited websites`, `100% data ownership`, `Email reports`];
+
+const list = document.querySelector(`.card__props__list`);
+
+items.forEach((el) => {
+  let listItem = document.createElement(`li`);
+
+  let imgItem = document.createElement(`img`);
+
+  imgItem.setAttribute(`src`, `./images/icon-check.svg`);
+
+  listItem.classList.add(`card__props__item`);
+
+  listItem.append(imgItem);
+
+  listItem.append(el);
+
+  list.append(listItem);
+});
