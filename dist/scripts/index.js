5d90dae550ce2e77d7505f7779f44aa5c76dd68a
abdalla rahmah <abdallarahmah.dev@gmail.com>
2024-02-04 05:22:14 +0400

    update project code base
    - delete src and dist folders with old code files.
    - delet .gitignore file.
    - delete package.json and package-lock.json files.
    - delete gulpfile.js.

--- dist/scripts/index.js
+++
@@ -1,7 +0,0 @@
-let rangeBtn = document.getElementById(`billings`);
-
-const price = document.getElementById(`price`);
-
-rangeBtn.addEventListener(`change`, (e) => {
-  console.log(rangeBtn.value);
-});
--- dist/scripts/plan-toggle.js
+++
@@ -1,8 +0,0 @@
-const planBtn = document.querySelector(`.card__plan--button`);
-
-const toggle = document.getElementById(`toggle`);
-
-planBtn.addEventListener(`click`, (e) => {
-  planBtn.classList.toggle(`active--btn`);
-  toggle.classList.toggle(`active--toggle`);
-});
